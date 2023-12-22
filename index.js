a6bed2f4f09daf155b6e6f597fe2506c23c5a8ed
MatthewLeaman <leaman.30@osu.edu>
2023-12-22 00:54:48 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function superbowlWin(array) {
+    let record = array;
+    function findWins(record) {
+        return record.result === "W";
+    }
+    if (array.find(findWins) !== undefined) {
+        return array.find(findWins).year;
+    }
+    else {
+        const sadReality = undefined;
+        return sadReality;
+    }
+}
+
